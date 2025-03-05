ecfacfd304ef9a1297b89690b5f7cd9120af8a27
waseem abu nada <waseemabunada202@gmail.com>
2025-03-05 12:54:25 +0200

    test

---
+++ Web Fundamentals/JS/loop Challenges.js
@@ -0,0 +1,62 @@
+
+//1-odd numbers
+/*
+   for(var i=1 ;i<=20;i++){
+    if(i%2 != 0){
+        console.log(i)
+    }
+   }
+    */
+   
+//2-Decreasing multiples of 3 
+/*
+   for(var i=100 ; i>=0 ;i-- ){
+    if (i%3 == 0){
+        console.log(i)
+    }
+   }
+*/
+
+//3-Print the given sequence 
+/*
+var start = 4;
+var end = -3.5;
+var step = -1.5;
+while (start >= end) {
+    console.log(start);
+    start =start + step;
+}
+    */
+   
+//4-sigma
+/*
+var usernumber=100;
+var sum=0;
+if(usernumber>0){
+    for(i=1; i<=usernumber ; i++){
+        sum +=i;
+    }
+    console.log(sum)
+}
+else{
+    console.log("error")
+}
+    */
+
+
+//5-fact
+/*
+var usernumber=12;
+var sum=1;
+if(usernumber>0){
+    for(i=1; i<=usernumber ; i++){
+        sum *=i;
+    }
+    console.log(sum)
+}
+else{
+    console.log("error")
+}
+   */
+  
+
