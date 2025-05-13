cacee423333942b011954802e5b6ae4251772eff
waseem abu nada <waseemabunada202@gmail.com>
2025-05-13 12:59:22 +0300

    FirstJavaProgram

---
+++ java/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("my name is waseem abu nada");
+        System.out.println("I am 23 years old");
+        System.out.println("My home town is Ramallah, Palestine");
+    }
+}
