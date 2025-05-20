14d7991eede6975da5a7dc05903186c730f89977
waseem abu nada <waseemabunada202@gmail.com>
2025-05-20 04:51:58 +0300

    puzzle

--- java/Puzzle/PuzzleJava.java
+++ java/Puzzle/PuzzleJava.java
@@ -1,37 +1,44 @@
 import java.util.Random;
 
 public class PuzzleJava {
-    public static int[] getTenRolls() {
-        int[] rolls = new int[10];
-        Random randMachine = new Random();
 
-        for (int i = 0; i < rolls.length; i++) {
-            rolls[i] = randMachine.nextInt(20) + 1; 
+    public int[] getTenRolls() {
+        int[] arr = new int[10];
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            int rand = r.nextInt(20) + 1;
+            arr[i] = rand;
         }
-
-        return rolls;
+        return arr;
     }
-}
 
+    public char getRandomLetter() {
+        char[] alphabet = new char[26];
 
+        for (int i = 0; i < 26; i++) {
+            alphabet[i] = (char) (i + 'a');
+        }
+        Random rand = new Random();
+        int index = rand.nextInt(26);
+        return alphabet[index];
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public String generatePassword(){
+        String password = "";
+        for (int i = 0; i < 8; i++) {
+            password += getRandomLetter();   
+        }
+        return password;
+    }
     
+    public String[] getNewPasswordSet(int length) {
+        String[] passwords = new String[length];
+        for (int i = 0; i < length; i++) {
+            passwords[i] = generatePassword();
+        }
+        return passwords;
+    }
+
+
+
+}
--- java/Puzzle/TestPuzzleJava.java
+++ java/Puzzle/TestPuzzleJava.java
@@ -1,44 +1,17 @@
-import java.util.ArrayList;
-import java.util.Random;
-
-
-
 public class TestPuzzleJava {
     public static void main(String[] args) {
         PuzzleJava appJava = new PuzzleJava();
 
-        
         int[] results = appJava.getTenRolls();
         for (int i = 0; i < results.length; i++) {
-        System.out.println(results[i]);
-}
+            System.out.println(results[i]);
+            System.out.println("Random letter: " + appJava.getRandomLetter());
+        }
+        System.out.println(appJava.generatePassword());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        String[] passwords = appJava.getNewPasswordSet(5);
+        for (int i = 0; i < passwords.length; i++) {
+            System.out.println("Generated Password: " + passwords[i]);
+        }
     }
 }
